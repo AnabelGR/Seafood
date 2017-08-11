6d4bd2cba2adb81c5fc403624337456b4d9c3f76
Anabel <anabel@apparentinc.com>
2017-08-11 13:14:54 -0700

    add passing controller test

---
+++ Seafood.Tests/ControllerTests/SubscriberControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Seafood.Controllers;
+using Seafood.Models;
+using Xunit;
+
+namespace Seafood.Tests.ControllerTests
+{
+    public class SubscriberControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            SubscriberController controller = new SubscriberController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
