02955126625f3c4441b8ac34687248863134da9e
Anabel <anabel@apparentinc.com>
2017-08-11 12:56:48 -0700

    Add new initial project, begin testing. Migrations issues resolved. First model test failed.

---
+++ Seafood.Tests/ModelTests/SubscriberTest.cs
@@ -0,0 +1,21 @@
+﻿using Seafood.Models;
+using Xunit;
+
+namespace Seafood.Tests
+{
+    public class SubscriberTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var subscriber = new Subscriber();
+
+            //Act
+            var result = subscriber.Name;
+
+            //Assert
+            Assert.Equal("Louis", result);
+        }
+    }
+}
