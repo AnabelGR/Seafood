682262a4d3771c24cf08ddfc35ff7b21789daba6
Anabel <anabel@apparentinc.com>
2017-08-11 13:10:58 -0700

    add passing subscriber model tests.

--- Seafood.Tests/ModelTests/SubscriberTest.cs
+++ Seafood.Tests/ModelTests/SubscriberTest.cs
@@ -6,16 +6,28 @@ namespace Seafood.Tests
     public class SubscriberTest
     {
         [Fact]
-        public void GetDescriptionTest()
+        public void GetNameTest()
         {
             //Arrange
             var subscriber = new Subscriber();
 
             //Act
-            var result = subscriber.Name;
+            var result = subscriber.Name = "Louis";
 
             //Assert
             Assert.Equal("Louis", result);
+        }
+        [Fact]
+        public void GetEmailTest()
+        {
+            //Arrange
+            var subscriber = new Subscriber();
+
+            //Act
+            var result = subscriber.Email = "Louis@louis.com";
+
+            //Assert
+            Assert.Equal("Louis@louis.com", result);
         }
     }
 }
