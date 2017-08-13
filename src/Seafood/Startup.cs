4e6d17bb6c109ffefda8cfc1f1d29c620ac6de2e
Anabel <anabel@apparentinc.com>
2017-08-13 09:52:17 -0700

    Add styling

--- src/Seafood/Startup.cs
+++ src/Seafood/Startup.cs
@@ -39,6 +39,8 @@ namespace Seafood
 
         public void Configure(IApplicationBuilder app)
         {
+
+            app.UseStaticFiles();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
